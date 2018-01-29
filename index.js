e9316f2f82de59c8a83895eee06fa100fabc65bc
nickk <kazantsev.nickolay@gmail.com>
2018-01-30 01:00:27 +0300

    Clear index file

--- index.js
+++ index.js
@@ -2,18 +2,11 @@
 
 const path = require('path');
 const program = require('commander');
-const git = require('nodegit');
-const fs = require('fs');
 
-const VersionInfo = require('./lib/version');
 const Output = require('./lib/output');
-const PackageInfo = require('./lib/package-info');
-const branchInfoFactory = require('./lib/branch-factory');
-
 const GitVersionInfo = require('./lib/git-version-info');
 
 const workDir = process.cwd();
-const gitVersionInfo = new GitVersionInfo(workDir);
 const version = require(path.join(__dirname, '/package.json')).version;
 
 program
@@ -23,6 +16,7 @@ program
   .option('-w, --write', 'write version into package.json')
   .parse(process.argv);
 
+  const gitVersionInfo = new GitVersionInfo(workDir);
   gitVersionInfo.getVersionInfo().then(({ version, package }) =>
   {
     const { json, teamcity, write } = program;
